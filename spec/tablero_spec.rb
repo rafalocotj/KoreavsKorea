7c5fdca293f00baef31e9f78f95b70ff002a9345
kleer <kleer@kleer-csd-box>
2018-02-16 15:52:02 -0500

    ataque

--- spec/tablero_spec.rb
+++ spec/tablero_spec.rb
@@ -22,5 +22,28 @@ describe Tablero do
 		result = tablero.colocar_barco("21")
 		expect(result).to eq tableroEsperado		
 	end
+
+	it "ataque fallido" do 
+
+		#arrange
+		tablero = Tablero.new
+		tableroEsperado = ['00000','0V000','00000','00000','00000']
+
+		#
+		result = tablero.ataque("22")
+		expect(result).to eq tableroEsperado		
+	end
+
+	it "ataque efectivo" do 
+
+		#arrange
+		tablero = Tablero.new
+		tableroEsperado = ['00000','0X000','00000','00000','00000']
+
+		#
+		tablero.colocar_barco("22")
+		result = tablero.ataque("22")
+		expect(result).to eq tableroEsperado		
+	end
 	
 end
